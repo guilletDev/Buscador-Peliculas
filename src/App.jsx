1975ea6d1d09a1c18ed262ebd86b34725968450c
guilletdev <guilletdev@gmail.com>
2024-04-25 14:18:05 -0300

    Agrego el fetching de datos

--- src/App.jsx
+++ src/App.jsx
@@ -44,12 +44,13 @@ function useSearch() {
 
 
 function App() {
-  const { movies } = useMovies();
-  const { search, updateSearch, error } = useSearch()
+  
+  const { search, updateSearch, error } = useSearch();
+  const { movies, getMovies } = useMovies({search});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(search);
+    getMovies()
   };
 
   const handleChange = (e) => {
--- src/hook/useMovies.js
+++ src/hook/useMovies.js
@@ -1,15 +1,25 @@
-import responseMovies from "../mocks/with-results.json";
+import whitResults from "../mocks/with-results.json";
 import withoutResults from "../mocks/no-results.json";
+import { useState } from "react";
 
-export const useMovies = () => {
+export const useMovies = ({ search }) => {
+  const [responseMovies, setResponseMovies] = useState([]);
   const movies = responseMovies.Search;
 
-  const mappedMovies = movies.map((movie) => ({
+  const mappedMovies = movies?.map((movie) => ({
     id: movie.imdbID,
     title: movie.Title,
     year: movie.Year,
     image: movie.Poster,
   }));
 
-  return { movies: mappedMovies };
+  const getMovies = () => {
+    if (search) {
+      fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=436d6473&s=${search}`)
+        .then((res) => res.json())
+        .then((json) => setResponseMovies(json));
+    } else setResponseMovies(withoutResults);
+  };
+
+  return { movies: mappedMovies, getMovies };
 };
